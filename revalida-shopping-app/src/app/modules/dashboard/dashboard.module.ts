28d37a284824e116cfdf551c78305f5274bac6d1
Juriel Botoy <juriel.botoy182@gmail.com>
2024-09-03 19:29:04 +0800

    Added profile page, navbar component

--- revalida-shopping-app/src/app/modules/dashboard/dashboard.module.ts
+++ revalida-shopping-app/src/app/modules/dashboard/dashboard.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }
